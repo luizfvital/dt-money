fef2b55073245fecd65c740fe72ae595997712c3
Luiz Fernando Vital <luizfvital@gmail.com>
2023-08-25 22:35:19 +0100

    add NewTransactionButton

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
+
+import logoImage from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImage} alt="" />
+
+        <NewTransactionButton>
+          Nova transação
+        </NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
