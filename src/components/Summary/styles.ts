2bc71ebd60d01a3046489929a23f03e3aca1756b
Luiz Fernando Vital <luizfvital@gmail.com>
2023-08-28 23:12:02 +0100

    add summary component

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+export const SummaryContainer = styled.section`
+  width: 100%;
+  max-width: 1120px;
+  margin: -5rem auto 0;
+  paddding: 0 1.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+`
