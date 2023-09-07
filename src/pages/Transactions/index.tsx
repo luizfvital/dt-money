1283a6bb8fe8159572e77d26e89b4a59790d8cfc
Luiz Fernando Vital <luizfvital@gmail.com>
2023-09-07 18:01:46 +0100

    format dates and prices

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,7 +3,8 @@ import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 import { SearchForm } from './components/SearchForm';
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles';
-import {TransactionsContext} from '../../contexts/transactionsContext'
+import {TransactionsContext} from '../../contexts/TransactionsContext'
+import {priceFormatter, dateFormatter} from '../../utils/formatter'
 
 
 
@@ -25,11 +26,12 @@ export function Transactions() {
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHighlight variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}  
                   </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
             </tr>
             ))}
           </tbody>
